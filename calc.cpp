3dc48fb1d9e8bf788bc83aec18e238dcc2e87e27
Nick Lu <nicklu@nicklu.acasmart.jh.edu>
2021-11-16 14:53:48 -0500

    Added implementation

--- calc.cpp
+++ calc.cpp
@@ -1,19 +1,24 @@
 #include "calc.h"
+#include <iostream>
 #include <string>
+#include <map>
+#include <vector>
+#include <sstream>
+#include <cctype>
 
 struct Calc {
 private:
     // fields
-
+    std::map<std::string, int> var_dict;
 public:
     // public member functions
     Calc();
     ~Calc();
 
     int evalExpr(const std::string &expr, int &result);
-
 private:
     // private member functions
+    
 };
 
 extern "C" struct Calc *calc_create(void) {
@@ -26,4 +31,26 @@ extern "C" void calc_destroy(struct Calc *calc) {
 
 extern "C" int calc_eval(struct Calc *calc, const char *expr, int *result) {
     return calc->evalExpr(expr, *result);
+}
+
+int Calc::evalExpr(const std::string &expr, int &result) {
+    std::vector<std::string> tokens = tokenize(expr);
+    int num_tokens = tokens.size();
+    if (num_tokens > 5 || num_tokens == 4 || num_tokens == 2 || num_tokens < 1)
+    {
+        return 0;
+    }
+    
+}
+
+std::vector<std::string> tokenize(const std::string &expr) {
+    std::vector<std::string> vec;
+    std::stringstream s(expr);
+
+    std::string tok;
+    while (s >> tok) {
+        vec.push_back(tok);
+    }
+
+    return vec;
 }
